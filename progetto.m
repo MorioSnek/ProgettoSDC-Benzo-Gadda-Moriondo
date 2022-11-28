9ae942913965023a33f51210adc78640a0308700
MorioSnek <luca.moriondo@me.com>
2022-11-28 16:12:05 +0100

    Aggiunta simulazioni e SNR

--- progetto.m
+++ progetto.m
@@ -6,7 +6,7 @@ clear all, close all, clc
 Pt_dBm = 0; % potenza trasmettitore [dBm]
 Gt_dB = 10; % guadagno antenna trasmettitore [dB]
 Gr_dB = 10; % guadagno antenna ricevitore [dB]
-Pn_dB = -85; % potenza del rumore [dBm]
+Pn_dBm = -85; % potenza del rumore [dBm]
 Fc = 28 * 10^9; % frequenza della portante [Hz]
 
 % parametri veicoli
@@ -24,18 +24,28 @@ DistSicurezza = 2.5; % distanza di sicurezza (distanza minima) [Km]
 
 %% System Model
 
-DistTxRx = [DistSicurezza:0.5:LungScenario]; % array di valori delle distanze considerate per la simulazione [m]
+DistTxRxFissa = 50;
 % Attenuazione in spazio libero
-MediaLoS = 32.4 + 20 * log10(DistTxRx/10^3) + 20 * log10(Fc/10^9); % converto la distanza in km e la frequenza in GHz
+MediaLoS = 32.4 + 20 * log10(DistTxRxFissa / 10^3) + 20 * log10(Fc / 10^9); % conversione m->Km e Hz->GHz
 % Shadowing component
 MediaSh = 0;
-VarianzaSh = 0;
+VarianzaSh = 3;
 % Attenuazione da bloccaggio
 MediaAtt = 0;
 VarianzaAtt = 0;
 % Attenuazione complessiva
 MediaPathLoss = MediaLoS + MediaAtt;
 VarianzaPathLoss = VarianzaSh + VarianzaAtt;
+% Modello PathLoss
+PathLoss = VarianzaPathLoss * randn(1, 100000) + MediaPathLoss;
+SNR = (Pt_dBm - 30) + Gt_dB + Gr_dB - PathLoss - (Pn_dBm - 30);
 
-% Modellazione ellissoide di Fresnel
-LungOnda = (3*10^9)/(28*10^9);
+%% Simulazioni numeriche
+
+histogram(PathLoss, 'BinWidth', 0.5)
+xlabel('dB');
+ylabel('Occorrenze nella simulazione');
+title('Confronto Path Loss e SNR');
+hold on
+histogram(SNR, 'BinWidth', 0.5)
+legend
