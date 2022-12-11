83ab22c7f5b789ea9ca1f45b89f0ad90c8d837e1
MorioSnek <luca.moriondo@me.com>
2022-12-11 17:32:00 +0100

    estensione pt3

--- progetto.m
+++ progetto.m
@@ -19,7 +19,9 @@ AltVeicoloStdv = 0.08; % deviazione standard delle altezze dei veicoli [m]
 % parametri scenario analizzato
 LungScenario = 200; % lunghezza dello scenario analizzato [m]
 NumCorsie = 3; % numero di corsie considerate nello scenario
-DensTraffico = 10; % densità a cui sono distribuiti i veicoli [veicolo/m]
+LarghezzaCorsia = 3.5;
+DensTraffico1 = 10; % densità a cui sono distribuiti i veicoli [veicolo/m]
+DensTraffico2 = 50;
 DistSicurezza = 2.5; % distanza di sicurezza (distanza minima) [m]
 
 %% System Model
@@ -38,31 +40,13 @@ MediaPathLoss = MediaLoS + MediaAtt;
 VarianzaPathLoss = VarianzaSh + VarianzaAtt;
 
 % Modello PathLoss
-NumSimulazioni = 10 ^ 6;
+NumSimulazioni = 10 ^ 4;
 PathLossLoS = VarianzaSh * randn(1, NumSimulazioni) + MediaLoS; % conversione Hz->GHz
 PathLossNLoSv = VarianzaPathLoss * randn(1, NumSimulazioni) + MediaPathLoss;
-% Simulazione numerica
-figure(1)
-histogram(PathLossLoS, 'BinWidth', 1)
-hold on
-histogram(PathLossNLoSv, 'BinWidth', 1)
-xlabel('dB');
-ylabel('Densità di Probabilità');
-title('Path loss caso LOS e NLoSv a dtr 50m');
-legend('LoS', 'NLoSv')
 
 % Modello SNR
 SNR = Pt_dBm + Gt_dB + Gr_dB - PathLossLoS - Pn_dBm;
 SNRNLoSv = Pt_dBm + Gt_dB + Gr_dB - PathLossNLoSv - Pn_dBm;
-% Simulazione numerica
-figure(2)
-histogram(SNR, 'BinWidth', 1)
-hold on
-histogram(SNRNLoSv, 'BinWidth', 1)
-xlabel('dB');
-ylabel('Densità di Probabilità');
-title('SNR caso LOS e NLoSv a dtr 50m');
-legend('LoS', 'NLoSv')
 
 % Modello PathLoss su distanza
 DistanzaTxRxMobile = [DistSicurezza:0.625:LungScenario];
@@ -70,46 +54,25 @@ PathLossMobileLoS = 32.4 + 20 * log10(DistanzaTxRxMobile) + 20 * log10(Fc / 10 ^
 SNRMobileLoS = Pt_dBm + Gt_dB + Gr_dB - PathLossMobileLoS - Pn_dBm;
 PathLossMobileNLoSv = PathLossMobileLoS + 9 + max(0, 15 * log10(DistanzaTxRxMobile / 2) - 41);
 SNRMobileNLoSv = Pt_dBm + Gt_dB + Gr_dB - PathLossMobileNLoSv - Pn_dBm;
-% Simulazione numerica
-figure(3)
-hold on
-grid on
-xlabel('Distanza dtr');
-ylabel('dB');
-title('Path Loss e SNR a distanza variabile');
-plot(DistanzaTxRxMobile, PathLossMobileLoS, 'LineWidth', 3);
-plot(DistanzaTxRxMobile, PathLossMobileNLoSv, 'LineWidth', 3);
-plot(DistanzaTxRxMobile, SNRMobileLoS, 'LineWidth', 3);
-plot(DistanzaTxRxMobile, SNRMobileNLoSv, 'LineWidth', 3);
-legend('Path loss LoS', 'Path loss NLoSv', 'SNR LoS', 'SNR NLoSv')
 
 % Simulazione vs Analitica SNR su distanza
 SimSNRMobileLoS = SNRMobileLoS + randn(1, size(DistanzaTxRxMobile, 2)) * VarianzaSh;
 SimSNRMobileNLoSv = SNRMobileNLoSv + randn(1, size(DistanzaTxRxMobile, 2)) * VarianzaPathLoss;
-% Simulazione numerica
-figure(4)
-subplot(2, 1, 1)
-hold on
-grid on
-xlim([0 200])
-ylim([-10 50])
-xlabel('Distanza dtr');
-ylabel('dB');
-title('Simulazione SNR LoS a distanza variabile');
-plot(DistanzaTxRxMobile, SNRMobileLoS, 'LineWidth', 3);
-stem(DistanzaTxRxMobile, SimSNRMobileLoS, 'filled', 'LineStyle', 'none');
-subplot(2, 1, 2)
-hold on
-grid on
-xlim([0 200])
-ylim([-20 40])
-xlabel('Distanza dtr');
-ylabel('dB');
-title('Simulazione SNR NLoSv a distanza variabile');
-plot(DistanzaTxRxMobile, SNRMobileNLoSv, 'LineWidth', 3);
-stem(DistanzaTxRxMobile, SimSNRMobileNLoSv, 'filled', 'LineStyle', 'none');
 
 %% Vehicular Blockage Modelling
+
+% Setup slot
+LarghezzaTotLane = NumCorsie * LarghezzaCorsia;
+LunghezzaSlotA = LungVeicolo + DistSicurezza; % Lunghezza degli slot di tipo A
+LunghezzaSlotB = (LargVeicolo * sqrt(DistanzaTxRxFissa ^ 2) -LarghezzaTotLane ^ 2) / (2 * LarghezzaTotLane); % Lunghezza degli slot di tipo B
+LunghezzaSlotC = 2 * LunghezzaSlotB + LungVeicolo; % Lunghezza degli slot di tipo C
+ProbSamelane = 1 / NumCorsie;
+ProbDifflane = 1 - ProbSamelane;
+NumeroMaxSlot = floor(DistanzaTxRxFissa / LunghezzaSlotA);
+% Variaibli PPP
+GammaA = DensTraffico1*10^-3 * LunghezzaSlotA;
+GammaB = DensTraffico1*10^-3 * LunghezzaSlotB;
+GammaC = DensTraffico1*10^-3 * LunghezzaSlotC;
 
 % Definizione distanze dal bloccante
 DistanzaTxB = DistanzaTxRxFissa / 2; % in questo caso collocato a metà tra TX e RX
@@ -117,14 +80,16 @@ DistanzaBRx = DistanzaTxB;
 
 % Modellazione primo ellissoide di Fresnel
 RaggioFresnel = sqrt(Lambda_c * (DistanzaTxB * DistanzaBRx) / (DistanzaTxB + DistanzaBRx));
-AltezzaFresnel = AltVeicoloStdv^2 * randn(1, NumSimulazioni) + (AltVeicoloMedia - 0.6 * RaggioFresnel);
+AltezzaFresnel = AltVeicoloStdv ^ 2 * randn(1, NumSimulazioni) + (AltVeicoloMedia - 0.6 * RaggioFresnel);
 
 % Probabilità Bloccaggio dato veicolo presente
-AltezzaBloccante = AltVeicoloStdv^2 * randn(1,NumSimulazioni) + AltVeicoloMedia;
-
+AltezzaBloccante = AltVeicoloStdv ^ 2 * randn(1, NumSimulazioni) + AltVeicoloMedia;
 AltezzaEfficace = AltezzaBloccante - AltezzaFresnel;
 MediaEfficace = 0.6 * RaggioFresnel;
+DevstdEfficace = sqrt(2 * AltVeicoloStdv ^ 2);
+Prob_NLoSv_B = qfunc((AltezzaEfficace - MediaEfficace) / DevstdEfficace); % CHIEDERE A FRANCESCO, NON TORNA
 
-% DevstdEfficace = 
-
-Prob_NLoSv_B = qfunc((AltezzaEfficace-MediaEfficace)/DevstdEfficace);
+% Probabilità Same Lane
+k = 3;
+ProbSingleSameLane = Prob_NLoSv_B * GammaA * exp(-GammaA);
+ProbMultiSameLane = (factorial(NumeroMaxSlot) / (factorial(NumeroMaxSlot - k) .* factorial(k))) .* ProbSingleSameLane .^ k .* (1-ProbSingleSameLane).^(NumeroMaxSlot-k);
