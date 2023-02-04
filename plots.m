0289e729fcd02ea902332b5c8681a548111642df
MorioSnek <luca.moriondo@me.com>
2023-02-04 16:52:05 +0100

    test: plots now not commented

--- plots.m
+++ plots.m
@@ -1,58 +1,58 @@
-% % Plot PathLoss LoS e NLoSv a 50m
-% figure(1)
-% histogram(PathLossLoS, 'BinWidth', 1)
-% hold on
-% histogram(PathLossNLoSv, 'BinWidth', 1)
-% xlabel('dB');
-% ylabel('Densità di Probabilità');
-% title('Path loss caso LOS e NLoSv a dtr 50m');
-% legend('LoS', 'NLoSv')
+%% Plot PathLoss LoS e NLoSv a 50m
+figure(1)
+histogram(PathLossLoS, 'BinWidth', 1)
+hold on
+histogram(PathLossNLoSv, 'BinWidth', 1)
+xlabel('dB');
+ylabel('Densità di Probabilità');
+title('Path loss caso LOS e NLoSv a dtr 50m');
+legend('LoS', 'NLoSv')
 
-% % Plot SNR LoS e NLoSv a 50m
-% figure(2)
-% histogram(SNR, 'BinWidth', 1)
-% hold on
-% histogram(SNRNLoSv, 'BinWidth', 1)
-% xlabel('dB');
-% ylabel('Densità di Probabilità');
-% title('SNR caso LOS e NLoSv a dtr 50m');
-% legend('LoS', 'NLoSv')
+%% Plot SNR LoS e NLoSv a 50m
+figure(2)
+histogram(SNR, 'BinWidth', 1)
+hold on
+histogram(SNRNLoSv, 'BinWidth', 1)
+xlabel('dB');
+ylabel('Densità di Probabilità');
+title('SNR caso LOS e NLoSv a dtr 50m');
+legend('LoS', 'NLoSv')
 
-% % Plot SNR e PathLoss a distanza variabile
-% figure(3)
-% hold on
-% grid on
-% xlabel('Distanza dtr');
-% ylabel('dB');
-% title('Path Loss e SNR a distanza variabile');
-% plot(DistanzaTxRxMobile, PathLossMobileLoS, 'LineWidth', 3);
-% plot(DistanzaTxRxMobile, PathLossMobileNLoSv, 'LineWidth', 3);
-% plot(DistanzaTxRxMobile, SNRMobileLoS, 'LineWidth', 3);
-% plot(DistanzaTxRxMobile, SNRMobileNLoSv, 'LineWidth', 3);
-% legend('Path loss LoS', 'Path loss NLoSv', 'SNR LoS', 'SNR NLoSv')
+%% Plot SNR e PathLoss a distanza variabile
+figure(3)
+hold on
+grid on
+xlabel('Distanza dtr');
+ylabel('dB');
+title('Path Loss e SNR a distanza variabile');
+plot(DistanzaTxRxMobile, PathLossMobileLoS, 'LineWidth', 3);
+plot(DistanzaTxRxMobile, PathLossMobileNLoSv, 'LineWidth', 3);
+plot(DistanzaTxRxMobile, SNRMobileLoS, 'LineWidth', 3);
+plot(DistanzaTxRxMobile, SNRMobileNLoSv, 'LineWidth', 3);
+legend('Path loss LoS', 'Path loss NLoSv', 'SNR LoS', 'SNR NLoSv')
 
-% % Simulazione numerica distribuzione SNR a 50m LoS e NLoSv
-% figure(4)
-% subplot(2, 1, 1)
-% hold on
-% grid on
-% xlim([0 200])
-% ylim([-10 50])
-% xlabel('Distanza dtr');
-% ylabel('dB');
-% title('Simulazione SNR LoS a distanza variabile');
-% plot(DistanzaTxRxMobile, SNRMobileLoS, 'LineWidth', 3);
-% stem(DistanzaTxRxMobile, SimSNRMobileLoS, 'filled', 'LineStyle', 'none');
-% subplot(2, 1, 2)
-% hold on
-% grid on
-% xlim([0 200])
-% ylim([-20 40])
-% xlabel('Distanza dtr');
-% ylabel('dB');
-% title('Simulazione SNR NLoSv a distanza variabile');
-% plot(DistanzaTxRxMobile, SNRMobileNLoSv, 'LineWidth', 3);
-% stem(DistanzaTxRxMobile, SimSNRMobileNLoSv, 'filled', 'LineStyle', 'none');
+%% Simulazione numerica distribuzione SNR a 50m LoS e NLoSv
+figure(4)
+subplot(2, 1, 1)
+hold on
+grid on
+xlim([0 200])
+ylim([-10 50])
+xlabel('Distanza dtr');
+ylabel('dB');
+title('Simulazione SNR LoS a distanza variabile');
+plot(DistanzaTxRxMobile, SNRMobileLoS, 'LineWidth', 3);
+stem(DistanzaTxRxMobile, SimSNRMobileLoS, 'filled', 'LineStyle', 'none');
+subplot(2, 1, 2)
+hold on
+grid on
+xlim([0 200])
+ylim([-20 40])
+xlabel('Distanza dtr');
+ylabel('dB');
+title('Simulazione SNR NLoSv a distanza variabile');
+plot(DistanzaTxRxMobile, SNRMobileNLoSv, 'LineWidth', 3);
+stem(DistanzaTxRxMobile, SimSNRMobileNLoSv, 'filled', 'LineStyle', 'none');
 
 %% Simulazione numerica probabilità bloccaggio a distanza variabile
 figure(5)
@@ -101,6 +101,7 @@ ylabel('Distanza d_{tr} [10^1m]')
 zlabel('Probabilità di bloccaggio')
 title('Probabilità di bloccaggio con densità veicolare e distanza variabili (Rooftop)')
 
+%% Simulazione numerica probabilità bloccaggio a densità e distanza variabile Bumper
 figure(8)
 surf(ProbNLoSDoppiaB)
 xlim([10 30])
@@ -109,5 +110,5 @@ zlim([0 0.8])
 xlabel('Densità veicolare [veh/km]')
 ylabel('Distanza d_{tr} [10^1m]')
 zlabel('Probabilità di bloccaggio')
-title('Probabilità di bloccaggio con densità veicolare e distanza variabili (Bumpers)')
+title('Probabilità di bloccaggio con densità veicolare e distanza variabili (Bumper)')
 
